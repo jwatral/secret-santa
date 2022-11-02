5f62a8b3df5562d5d8b4649302450afef5205b30
Jaroslaw Watral <jwatral@users.noreply.github.com>
2022-11-02 20:55:05 +0100

    intial setup, empty spring boot project

---
+++ src/main/kotlin/dev/jwatral/secretsanta/SecretSantaApplication.kt
@@ -0,0 +1,11 @@
+package dev.jwatral.secretsanta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SecretSantaApplication
+
+fun main(args: Array<String>) {
+    runApplication<SecretSantaApplication>(*args)
+}
